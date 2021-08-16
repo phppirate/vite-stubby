80c9a8f471ad1eef772403b92ec4b87da67d0239
Sam Podlogar <sam@podlogar.com>
2021-08-16 14:13:37 -0600

    Update Guest Stub to include Vite Assets

--- src/stubs/views/layouts/guest.blade.php
+++ src/stubs/views/layouts/guest.blade.php
@@ -7,14 +7,7 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        {{ vite_assets(true) }}
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
